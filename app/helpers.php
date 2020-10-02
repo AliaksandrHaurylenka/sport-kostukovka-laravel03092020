e00521d4cb5d92c6bdada270cc09463d3fa11a15
AliaksandrHaurylenka <goric0312@mail.ru>
2020-10-02 11:50:05 +0300

    asd

--- app/helpers.php
+++ app/helpers.php
@@ -1,11 +1,25 @@
 <?php
 
-    function getLengthString($string, $length)
-    {
-        $string = strip_tags($string);
-        $string = mb_substr($string, 0, $length);
-        $string = rtrim($string, "!,.-");
-        $string = mb_substr($string, 0, strrpos($string, ' '));
+function getLengthString($string, $length)
+{
+    $string = strip_tags($string);
+    $string = mb_substr($string, 0, $length);
+    $string = rtrim($string, "!,.-");
+    $string = mb_substr($string, 0, strrpos($string, ' '));
 
-        return $string.'...';
-    }
+    return $string . '...';
+}
+
+/**
+ * mb_ucfirst - преобразует первый символ в верхний регистр
+ * @param string $str - строка
+ * @param string $encoding - кодировка, по-умолчанию UTF-8
+ * @return string
+ */
+function mb_ucfirst($str, $encoding = 'UTF-8')
+{
+    $str = mb_ereg_replace('^[\ ]+', '', $str);
+    $str = mb_strtoupper(mb_substr($str, 0, 1, $encoding), $encoding) .
+        mb_substr($str, 1, mb_strlen($str), $encoding);
+    return $str;
+}
